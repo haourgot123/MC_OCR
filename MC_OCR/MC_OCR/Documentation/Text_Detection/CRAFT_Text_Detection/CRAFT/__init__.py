fba0309a13baf887e1ba4e0cccd43e5daec9add5
Nguyễn Nguyên Hảo <haourgot123@gmail.com>
2024-10-26 22:20:18 +0700

    update

--- MC_OCR/MC_OCR/Documentation/Text_Detection/CRAFT_Text_Detection/CRAFT/__init__.py
+++
@@ -1,2 +0,0 @@
-from .model import CRAFTModel, preprocess_image
-from .utils import draw_boxes, draw_polygons, boxes_area, polygons_area
--- MC_OCR/MC_OCR/Documentation/Text_Detection/CRAFT_Text_Detection/CRAFT/basenet/__init__.py
+++
@@ -1,1 +0,0 @@
-from .vgg16_bn import vgg16_bn, init_weights
